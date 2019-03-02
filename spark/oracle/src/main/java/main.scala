35c54bf473429831d1dc60d531cb0f1cf2058812
shlian <shaohua.lian@xuncetech.com>
2019-03-02 11:48:49 +0800

    add comment

--- spark/oracle/src/main/java/main.scala
+++ spark/oracle/src/main/java/main.scala
@@ -8,12 +8,12 @@ object main {
     def main(args:Array[String])={
         val conf=new SparkConf().setMaster("local").setAppName("oracle_example")
         val spark=SparkSession.builder().config(conf).getOrCreate()
-        val connectionStr="jdbc:oracle:thin:@192.168.0.189:1521:ogg1"
+        val connectionStr="jdbc:oracle:thin:@192.168.0.189:1521:ogg1" //ogg1是数据库
         val properties=new Properties()
-        properties.put("user","ogg")
-        properties.put("password","xc_ogg@2019")
+        properties.put("user","ogg") //用户名
+        properties.put("password","xc_ogg@2019")//密码
         
-        val df:DataFrame=spark.sqlContext.read.jdbc(connectionStr,"snapshot",properties)
+        val df:DataFrame=spark.sqlContext.read.jdbc(connectionStr,"snapshot",properties) //表名
         df.printSchema()
         df.show()
     
